6f8b2528e38fb41a66aa69a9d7c65a0dce922481
Nicolas Romero <nicolas.romero1701@alumnos.ubiobio.cl>
2022-11-17 20:26:01 -0300

    Añadiendo paginacion, tamaño de pagina, busqueda y demás

--- src/app/components/lista-productos/lista-productos.component.ts
+++ src/app/components/lista-productos/lista-productos.component.ts
@@ -12,7 +12,15 @@ export class ListaProductosComponent implements OnInit {
 
   products: Producto[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   searchMode: boolean = false;
+
+  //nuevas propiedades para la paginacipn de productos
+  thePageNumber: number = 1;
+  thePageSize: number = 5;
+  theTotalElemnts: number = 0;
+  
+  previousKeyword: string = "";
 
 
   constructor(private productService: ProductoServicioService,
@@ -39,12 +47,21 @@ export class ListaProductosComponent implements OnInit {
     
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
   
+    // si tenemos una palabra en el buscador diferente que el anterior
+    // entonces queremos establecer el numero de pagina en 1
+
+     if(this.previousKeyword != theKeyword){
+      this.thePageNumber = 1;
+     }
+     this.previousKeyword = theKeyword;
+
+     console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
+
     //busqueda de productos usando la palabra clave dada
-    this.productService.searchProductos(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.searchProductsPaginate(this.thePageNumber -1,
+                                               this.thePageSize,
+                                               theKeyword).subscribe(this.processResult());
 
 
   }
@@ -61,12 +78,42 @@ export class ListaProductosComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
+
+    //verificar si tenemos un categoryid diferente
+    //NOTA: ANGULAR REUTILIZARA EL COMPONENTE
+
+
+    //si tenemos un categoryid diferene al anterior, reestablecemos el numero de pag a 1.
+    if(this.previousCategoryId != this.currentCategoryId){
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
+
     //senecesita obtener los productos
 
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.getProductListPaginate(this.thePageNumber - 1,
+                                              this.thePageSize,
+                                              this.currentCategoryId)
+                                              .subscribe(this.processResult());
   }
+
+  updatePageSize(pageSize: string){
+    this.thePageSize = parseInt(pageSize);
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+
+  processResult(){
+    return (data: any) =>{
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElemnts = data.page.totalElements;
+    }
+  }
+
 }
--- src/app/services/producto-servicio.service.ts
+++ src/app/services/producto-servicio.service.ts
@@ -9,37 +9,62 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductoServicioService {
- 
+
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
- 
+
 
   constructor(private httpClient: HttpClient) {
-   }
-   
-    getProduct(theProductId: number): Observable<Producto> {
-    
-      //construir url basada en el id del producto
-      const producturl= `${this.baseUrl}/${theProductId}`;
-
-      return this.httpClient.get<Producto>(producturl);
   }
 
-   getProductList(theCategoryId: number): Observable<Producto[]>{
-   
+  getProduct(theProductId: number): Observable<Producto> {
+
+    //construir url basada en el id del producto
+    const producturl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Producto>(producturl);
+  }
+  getProductListPaginate(thePage: number,
+    thePageSize: number,
+    theCategoryId: number): Observable<GetResponseProducts> {
+
     //POR HACER SI ES QUE LO LEES GIO: se necesita construir una url basada en el id de categoria 
-    
+    //pagina y tamaño
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+
+  }
+
+  getProductList(theCategoryId: number): Observable<Producto[]> {
+
+    //POR HACER SI ES QUE LO LEES GIO: se necesita construir una url basada en el id de categoria 
+
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     return this.getProducts(searchUrl);
-    
+
   }
 
   searchProductos(theKeyword: string): Observable<Producto[]> {
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
 
     return this.getProducts(searchUrl);
+  }
+
+  searchProductsPaginate(thePage: number,
+                          thePageSize: number,
+                          theKeyword: string): Observable<GetResponseProducts> {
+
+    //POR HACER SI ES QUE LO LEES GIO: se necesita construir una url basada en el id de categoria 
+    //pagina y tamaño
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+
   }
 
   private getProducts(searchUrl: string): Observable<Producto[]> {
@@ -49,21 +74,27 @@ export class ProductoServicioService {
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
-   
-    return this.httpClient.get< GetResponseProductCategory >(this.categoryUrl).pipe(
+
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
-     );
+    );
   }
 
 }
 
-interface GetResponseProducts{
+interface GetResponseProducts {
   _embedded: {
     products: Producto[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
-interface GetResponseProductCategory{
+interface GetResponseProductCategory {
   _embedded: {
     productCategory: ProductCategory[];
   }
